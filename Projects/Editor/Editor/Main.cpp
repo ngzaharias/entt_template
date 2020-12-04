0a55824a2a5a9ab9e469590088af9a090a532a1e
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2020-12-04 11:00:53 -0500

    Added experimental code to main.cpp

--- Projects/Editor/Editor/Main.cpp
+++ Projects/Editor/Editor/Main.cpp
@@ -1,11 +1,19 @@
 #include "Editor/Application.h"
 
+#include "Editor/Experimental/experimental_containers.h"
+#include "Editor/Experimental/experimental_pod.h"
+#include "Editor/Experimental/experimental_variant.h"
+
 #include <direct.h>
-#include <entt/entt.hpp>
 
 int main(int agrc, char* argv[])
 {
+	// #todo: we shouldn't need to change directory
 	_chdir("../../");
+
+	//experimental_containers();
+	//experimental_pod();
+	//experimental_variant();
 
 	editor::Application* application = new editor::Application();
 	application->Execute(agrc, argv);
