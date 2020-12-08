4c68bd2f66b9590fbd71ee7ad938f24715a64591
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2020-12-08 10:32:06 -0500

    Switched to Vector3

--- Projects/Engine/Engine/TransformComponent.h
+++ Projects/Engine/Engine/TransformComponent.h
@@ -1,18 +1,19 @@
 #pragma once
 
-#include "Engine/FieldAttributes.h"
+#include <Engine/FieldAttributes.h>
+#include <Engine/Vector3.h>
 
 #include <entt/entt.hpp>
 #include <refl/refl.hpp>
-#include <SFML/System/Vector3.hpp>
 
 namespace core
 {
 	struct TransformComponent
 	{
-		sf::Vector3f m_Translate = { 0.f, 0.f, 0.f };
-		sf::Vector3f m_Rotate = { 0.f, 0.f, 0.f };
-		sf::Vector3f m_Scale = { 1.f, 1.f, 1.f };
+		Vector3f m_Translate = { 0.f, 0.f, 0.f };
+		Vector3f m_Rotate = { 0.f, 0.f, 0.f };
+		Vector3f m_Scale = { 1.f, 1.f, 1.f };
+		int32 m_MyInt;
 	};
 }
 
