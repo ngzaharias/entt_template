2e3045449d1d750e3df67c3b419f669dd863dc63
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2020-12-08 10:31:59 -0500

    Added PCH (not used)

---
+++ Projects/Engine/Engine/EnginePCH.cpp
@@ -0,0 +1,1 @@
+#include "Engine/EnginePCH.h"
---
+++ Projects/Engine/Engine/EnginePCH.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// #note: The types of includes that are allowed in here...
+// - Common Containers
+// - Math
+// - String
+// - Types
+
+#include "Engine/MathHelpers.h"
+#include "Engine/Name.h"
+#include "Engine/Path.h"
+#include "Engine/String.h"
+#include "Engine/StringHelpers.h"
+#include "Engine/StringView.h"
+#include "Engine/Vector2.h"
+#include "Engine/Vector3.h"
+#include "Engine/VectorHelpers.h"
+#include "Engine/Types.h"
+
+#include <map>
+#include <set>
+#include <variant>
+#include <vector>
