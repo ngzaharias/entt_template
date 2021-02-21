a6a643bd4f812432d03b018e6e90a89d2b3c6d1a
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2021-02-21 00:56:36 -0500

    Finished Engine premake

--- Projects/Engine/premake5.lua
+++ Projects/Engine/premake5.lua
@@ -1,3 +1,27 @@
 project "Engine"
 	kind "StaticLib"
-	location "Engine"
+	defines { "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING" }
+	pchheader "EnginePCH.h"
+	pchsource "Engine/EnginePCH.cpp"
+	dependson { "Imgui" }
+
+	files 
+	{ 
+		"%{prj.location}/**.h",
+		"%{prj.location}/**.cpp",
+		"%{prj.location}/**.inl",
+	}
+
+	includedirs 
+	{ 
+		"%{wks.location}/3rdParty/",
+		"%{wks.location}/3rdParty/PhysX/Include/",
+		"%{wks.location}/3rdParty/SFML/Include/",
+		"%{wks.location}/Projects/Engine/",
+		"%{wks.location}/Projects/Imgui/",
+	}
+
+	filter "configurations:Debug"
+		defines { "_DEBUG" }
+	filter "configurations:Release"
+		defines { "NDEBUG" }
