7dd839ff5884eccd9518b75ecb1345a7d74072eb
Nicholas Zaharias <ngzaharias.logins@gmail.com>
2021-02-21 00:37:10 -0500

    Finished Imgui premake

--- Projects/Imgui/premake5.lua
+++ Projects/Imgui/premake5.lua
@@ -1,3 +1,26 @@
 project "Imgui"
 	kind "StaticLib"
-	location "Imgui"
+	files 
+	{ 
+		"%{prj.location}/**.h",
+		"%{prj.location}/**.cpp",
+		"%{prj.location}/**.inl",
+		"%{wks.location}/3rdParty/imgui/*.h",
+		"%{wks.location}/3rdParty/imgui/*.cpp",
+		"%{wks.location}/3rdParty/imgui/*.inl",
+		"%{wks.location}/3rdParty/imgui-sfml/*.h",
+		"%{wks.location}/3rdParty/imgui-sfml/*.cpp",
+		"%{wks.location}/3rdParty/imgui-sfml/*.inl",
+		"%{wks.location}/3rdParty/ImGuizmo/*.h",
+		"%{wks.location}/3rdParty/ImGuizmo/*.cpp",
+		"%{wks.location}/3rdParty/ImGuizmo/*.inl",
+	}
+	includedirs 
+	{ 
+		"%{wks.location}/3rdParty/",
+		"%{wks.location}/3rdParty/imgui/",
+		"%{wks.location}/3rdParty/imgui-sfml/",
+		"%{wks.location}/3rdParty/ImGuizmo/",
+		"%{wks.location}/3rdParty/SFML/Include/",
+		"%{wks.location}/Projects/Imgui/",
+	}
